de3feff509f17b658964cf78426b39e17d9e4093
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-04-26 18:28:15 +0530

    eslint added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
@@ -8,4 +8,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
