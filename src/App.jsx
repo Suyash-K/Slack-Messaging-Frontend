899b66a16fe253a7cae328c1f2b403129c17211c
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-05-04 15:22:08 +0530

    added signinContainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Route, Routes } from 'react-router-dom';
 
-import { SigninCard } from '@/components/organisms/Auth/SigninCard';
 import { Toaster } from '@/components/ui/sonner';
 import { Auth } from '@/pages/Auth/Auth';
 
+import { SigninContainer } from './components/organisms/Auth/SigninContainer';
 import { SignupContainer } from './components/organisms/Auth/SignupContainer';
 import { NotFound } from './pages/NotFound';
 function App() {
@@ -17,8 +17,8 @@ const queryClient = new QueryClient();
     <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
-        <Route path="/auth/signin" element={<Auth><SigninCard /></Auth>} />
-
+        <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
+        <Route path="/home" element={<Auth><h1>Home</h1></Auth>} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
       <Toaster />
---
+++ src/hooks/apis/auth/useSignin.js
@@ -0,0 +1,39 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+import { signInRequest } from '@/apis/auth';
+
+export const useSignin = () => {
+    const { isPending, isSuccess, error, mutateAsync: signinMutation } = useMutation({
+        mutationFn: signInRequest,
+        onSuccess: (data) => {
+            console.log('Successfully signed in', data);
+            toast.success('Successfully signed in', {
+                description: 'You will be redirected to the home page in a few seconds',
+                style: {
+                    background: 'var(--success)',
+                    color: 'var(--success-foreground)',
+                },
+                position: 'top-center'
+            });
+        },
+        onError: (error) => {
+            console.error('Failed to sign in', error);
+            toast.warning('Failed to sign in', {
+                            description: error?.response?.data?.message || 'Something went wrong',
+                            style: {
+                                background: 'var(--destructive)',
+                                color: 'var(--destructive-foreground)',
+                            },
+                            position: 'top-center'
+                        });
+        }
+    });
+
+    return {
+        isPending,
+        isSuccess,
+        error,
+        signinMutation
+    };
+};
