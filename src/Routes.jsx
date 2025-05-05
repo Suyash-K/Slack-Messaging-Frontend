586073e414378c57e5b80711c7e306dde111d50e
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-05-05 19:17:36 +0530

    added user button

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,13 +6,14 @@ import { Auth } from '@/pages/Auth/Auth';
 import { NotFound } from '@/pages/NotFound';
 
 import { ProtectedRoute } from './components/molecules/ProtectedRoute/ProtectedRoute';
+import { Home } from './pages/Home/Home';
 
 export const AppRoutes = () => {
     return (
         <Routes>
           <Route path="/auth/signup" element={<Auth><SignupContainer /></Auth>} />
           <Route path="/auth/signin" element={<Auth><SigninContainer /></Auth>} />
-          <Route path="/home" element={<ProtectedRoute><Auth><h1>Home</h1></Auth></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
     );
